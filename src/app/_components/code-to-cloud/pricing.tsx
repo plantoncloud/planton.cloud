ab637f7391b3952d6266ad53327c8911daf963b1
Satish-Lakhani <satishlakhani11@gmail.com>
2024-05-29 19:38:30 +0530

    Responsive related changes.

--- src/app/_components/code-to-cloud/pricing.tsx
+++ src/app/_components/code-to-cloud/pricing.tsx
@@ -5,7 +5,7 @@ import { plans } from '@/app/_utils/constants';
 
 export const Pricing: FC = () => {
   return (
-    <Box className="mt-2 md:mt-20 pt-16 md:pt-36 pb-48 md:pb-96 bg-slate-950 relative z-10">
+    <Box className="mt-2 md:mt-20 pt-16 md:pt-36 pb-40 md:pb-96 bg-slate-950 relative z-10">
       <Stack className="mx-auto px-5 items-center">
         <Box className="md:max-w-7xl md:mx-auto">
           <Chip className="hidden md:block px-6">Pricing</Chip>
@@ -27,9 +27,9 @@ export const Pricing: FC = () => {
             </Grid>
           </Box>
         </Box>
-        <Box className="w-[85%] aspect-square origin-top-left opacity-10 mix-blend-screen bg-gradient-to-b from-transparent from-50% to-indigo-950 to-90% rounded-full blur-sm absolute mx-auto -top-[700px]" />
-        <Box className="w-[95%] aspect-square origin-top-left opacity-20 mix-blend-screen bg-gradient-to-b from-transparent from-50% to-indigo-800 to-90% rounded-full blur-sm absolute mx-auto -top-[650px]" />
-        <Box className="w-[105%] aspect-square origin-top-left opacity-40 mix-blend-screen bg-gradient-to-b from-transparent from-50% to-purple-600 to-90% rounded-full blur-sm absolute mx-auto -top-[600px]" />
+        <Box className="w-[120%] md:[85%] aspect-square origin-top-left opacity-10 mix-blend-screen bg-gradient-to-b from-transparent from-50% to-indigo-950 to-90% rounded-full blur-sm absolute mx-auto bottom-60 md:bottom-[500px]" />
+        <Box className="w-[125%] md:[95%] aspect-square origin-top-left opacity-20 mix-blend-screen bg-gradient-to-b from-transparent from-50% to-indigo-800 to-90% rounded-full blur-sm absolute mx-auto bottom-32 md:bottom-72" />
+        <Box className="w-[130%] md:[105%] aspect-square origin-top-left opacity-40 mix-blend-screen bg-gradient-to-b from-transparent from-50% to-purple-600 to-90% rounded-full blur-sm absolute mx-auto bottom-4 md:bottom-20" />
         <Box className="w-[696px] h-[696px] bg-orange-800 rounded-full absolute -right-44 top-92 blur-3xl opacity-20" />
       </Stack>
       <Box className="mt-6 md:hidden">
