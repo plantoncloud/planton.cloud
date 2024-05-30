603588588f0e6550074de46492f6587e574c11c4
Satish-Lakhani <satishlakhani11@gmail.com>
2024-05-30 20:40:37 +0530

    - Minor change added in the homepage.

--- src/app/_components/home/client/layout-container.tsx
+++ src/app/_components/home/client/layout-container.tsx
@@ -5,5 +5,6 @@ import { Box, styled } from '@mui/material';
 export const StyledLayoutContainer = styled(Box)(() => ({
   backgroundImage:
     'linear-gradient(20deg, rgba(0,0,0,1) 0%, transparent 75%, rgba(0,0,0,0) 100%), linear-gradient(90deg, rgba(0,0,0,1) 0%, transparent 20%, rgba(0,0,0,0) 100%), url(/images/home/home-bg.png)',
+  backgroundPosition: 'top',
   left: 0,
 }));
